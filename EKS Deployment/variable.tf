7e39ab477b5cc176fa274c86d7f348a7ecc92ca1
arulmurugan001 <arulmurugan.cinna@gmail.com>
2025-04-26 17:46:25 +0530

    Add files via upload

---
+++ EKS Deployment/variable.tf
@@ -0,0 +1,11 @@
+variable "image_mutablity" {
+  description = "mutability"
+  type        = string
+  default     = "mutable"
+}
+# variable "encryption_type" {
+#   description = "encryption"
+#   type        = string
+#   default     = kms
+# }
+
